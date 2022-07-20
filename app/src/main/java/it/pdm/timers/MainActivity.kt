aa1683e40ce92bcf3f07835a5d3e47d0bc92a27e
lperf <l.perfetti2001@gmail.com>
2022-07-20 11:04:48 +0200

    created and put into operation the spinner

--- app/src/main/java/it/pdm/timers/MainActivity.kt
+++ app/src/main/java/it/pdm/timers/MainActivity.kt
@@ -2,10 +2,19 @@ package it.pdm.timers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lateinit var  option : Spinner
+        option = findViewById(R.id.sp_opinion)
+
+        val options = arrayOf(1,2,3,4,5,6,7,8,9,10)
+
+        option.adapter = ArrayAdapter<Int>(this,android.R.layout.simple_list_item_1, options)
     }
 }
