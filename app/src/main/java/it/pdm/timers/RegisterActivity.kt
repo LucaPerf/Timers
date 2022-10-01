1e0ecd908ee45822148ffc58a19b88a4c91ec675
LucaPerf <l.perfetti2001@gmail.com>
2022-10-01 16:41:00 +0200

    implemented HaveAnAccount button and Register button

--- app/src/main/java/it/pdm/timers/RegisterActivity.kt
+++ app/src/main/java/it/pdm/timers/RegisterActivity.kt
@@ -4,11 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        val tv_Login = findViewById<TextView>(R.id.tv_gia_account)
+
+        tv_Login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btn_Register = findViewById<Button>(R.id.btn_register)
+
+        btn_Register.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
