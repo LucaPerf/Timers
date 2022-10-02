0e77cd7e15effcc8babe9eaf00d39400debe848f
LucaPerf <l.perfetti2001@gmail.com>
2022-10-02 18:20:29 +0200

    created and implemented main activity

---
+++ app/src/main/java/it/pdm/timers/TimerActivity.kt
@@ -0,0 +1,11 @@
+package it.pdm.timers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+    }
+}
