d62a4be37f5015c36695331172e0d1bdcf15ac04
LucaPerf <l.perfetti2001@gmail.com>
2022-11-25 21:50:59 +0100

    implemented onCreatedOptionsMenu

--- app/src/main/java/it/pdm/timers/CountdownActivity.kt
+++ app/src/main/java/it/pdm/timers/CountdownActivity.kt
@@ -2,10 +2,16 @@ package it.pdm.timers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class CountdownActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_countdown)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_settings, menu)
+        return true
     }
 }
