7e27da4d4aff308f3baa4721328d1fc6d52fd851
LucaPerf <l.perfetti2001@gmail.com>
2022-11-28 00:17:12 +0100

    implemented progressbar for btn_avvio

--- app/src/main/java/it/pdm/timers/CountdownActivity.kt
+++ app/src/main/java/it/pdm/timers/CountdownActivity.kt
@@ -3,47 +3,109 @@ package it.pdm.timers
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.view.Menu
-import kotlinx.android.synthetic.main.activity_countdown.*
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import java.util.*
 
 class CountdownActivity : AppCompatActivity() {
-    enum class TimerState{
-        Stopped, Paused, Running
+    var txt_time: TextView? = null
+    var txt_breaks: TextView? = null
+    var img_play: ImageView? = null
+    var progressbar: ProgressBar? = null
+    var timerLength: Int? = null
+    var breaksCount: Int = 0
+    var totalbreaks: Int? = null
+
+    private enum class TimerState{
+        STARTED, STOPPED
     }
 
-    private lateinit var timer: CountDownTimer
-    private var timerLengthSeconds : Long = 0
-    private var timerState = TimerState.Stopped
-    private var secondsRemaining = 0L
+    private var timerState = TimerState.STARTED
+    var timeLeftInMillis: Long = 1 * 60000
+    lateinit var countDownTimer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_countdown)
+        init()
+        getData()
 
-        fab_start.setOnClickListener{v ->
-            startTimer()
-            timerState = TimerState.Running
-            updateButtons()
+        img_play!!.setOnClickListener{
+            if(timerState == TimerState.STARTED){
+                pauseTimer()
+            }else{
+                startTimer()
+            }
         }
 
-        fab_pause.setOnClickListener{v ->
-            timer.cancel()
-            timerState = TimerState.Paused
-            updateButtons()
-        }
-
-        fab_start.setOnClickListener{v ->
-            timer.cancel()
-            onTimerFinished()
-        }
+        //start with time and progressbar complete
+        updateCountDownText()
+        setProgressBarValues()
     }
 
-    override fun onResume() {
-        super.onResume()
+    fun init(){
+        txt_time = findViewById(R.id.tv_countdown)
+        txt_breaks = findViewById(R.id.txt_total_breaks)
+        progressbar = findViewById(R.id.progress_coundown)
+        img_play = findViewById(R.id.iv_play)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu):Boolean {
-        menuInflater.inflate(R.menu.menu_settings, menu)
-        return true
+    fun getData(){
+        timerLength = intent.getIntExtra("TIME", 1)
+        totalbreaks = intent.getIntExtra("BREAKS", 0)
+
+        if(totalbreaks == 0){
+            txt_breaks!!.visibility = View.GONE
+        }else{
+            txt_breaks!!.setText(breaksCount.toString() + "/" + totalbreaks.toString())
+        }
+
+        timeLeftInMillis = (timerLength!! * 60 * 1000).toLong()
+        startTimer()
+    }
+
+    private fun setProgressBarValues(){
+        //errore da guardare il setMax
+        progressbar!!.setMax(timeLeftInMillis.toInt() / 1000)
+
+        progressbar!!.setProgress(timeLeftInMillis.toInt() / 1000)
+    }
+
+    private fun startTimer(){
+
+        //possibile errore
+        countDownTimer = object : CountDownTimer(timeLeftInMillis, 1000){
+            override fun onTick(millisUntilFinished: Long){
+                timeLeftInMillis = millisUntilFinished
+                img_play!!.setImageResource(R.drawable.ic_pause)
+                updateCountDownText()
+                progressbar!!.setProgress((millisUntilFinished/1000).toInt())
+            }
+
+            override fun onFinish(){
+                timerState = TimerState.STOPPED
+            }
+        }.start()
+        timerState = TimerState.STARTED
+    }
+
+    private fun pauseTimer(){
+        countDownTimer.cancel()
+        timerState = TimerState.STOPPED
+        img_play!!.setImageResource(R.drawable.ic_play)
+
+    }
+
+    private fun updateCountDownText(){
+        val seconds: Int = (timeLeftInMillis.toInt() / 1000) / 60
+        val minutes: Int = (timeLeftInMillis.toInt() / 1000) % 60
+        val hours: Int = (timeLeftInMillis.toInt() / 1000) % 60
+
+        val timeLeftFormatted: String =
+            java.lang.String.format(Locale.getDefault(),
+            "%02d:%02d", hours, minutes, seconds)
+        txt_time!!.setText(timeLeftFormatted)
     }
 }
