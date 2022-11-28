5997b7e9a300672b7e429ee00232cb55bb8a1d3d
LucaPerf <l.perfetti2001@gmail.com>
2022-11-28 22:17:31 +0100

    created and implemented the openFunction method, onAddButtonClicked method and setAnimation method and setVisibility method

--- app/src/main/java/it/pdm/timers/fragments/TimerFragment.kt
+++ app/src/main/java/it/pdm/timers/fragments/TimerFragment.kt
@@ -1,10 +1,12 @@
 package it.pdm.timers.fragments
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import it.pdm.timers.*
 import kotlinx.android.synthetic.main.fragment_timer.*
 
@@ -15,6 +17,13 @@ import kotlinx.android.synthetic.main.fragment_timer.*
  */
 class TimerFragment : Fragment() {
     private lateinit var userArrayList : ArrayList<Timer>
+
+    private val open : Animation by lazy { AnimationUtils.loadAnimation(context, R.anim.rotate_open_anim) }
+    private val close : Animation by lazy { AnimationUtils.loadAnimation(context, R.anim.rotate_close_anim) }
+    private val fromBottom : Animation by lazy { AnimationUtils.loadAnimation(context, R.anim.from_bottom_anim) }
+    private val toBottom : Animation by lazy { AnimationUtils.loadAnimation(context, R.anim.to_bottom_anim) }
+
+    private var clicked = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,16 +50,69 @@ class TimerFragment : Fragment() {
 
         lv_timer.adapter = Adapter(this.requireActivity(), userArrayList)
 
-        /*btn_set_time.setOnClickListener {
+        openFunction(view)
+
+    }
+
+    private fun openFunction(view: View) {
+        val fab = view?.findViewById<FloatingActionButton>(R.id.fab)
+        val fabAdd = view?.findViewById<FloatingActionButton>(R.id.fab_add)
+        val fabPlay = view?.findViewById<FloatingActionButton>(R.id.fab_play)
+        val fabSave = view?.findViewById<FloatingActionButton>(R.id.fab_save)
+
+        fab.setOnClickListener {
+            onAddButtonClicked()
+        }
+
+        fabAdd.setOnClickListener {
             val intent = Intent(this.requireContext(), SetTimeActivity::class.java)
             startActivity(intent)
         }
 
-        btn_avvio.setOnClickListener {
+        fabPlay.setOnClickListener {
             val intent = Intent(this.requireContext(), CountdownActivity::class.java)
             startActivity(intent)
-        }*/
+        }
+    }
 
+    private fun onAddButtonClicked(){
+        setVisibility(clicked)
+        setAnimation(clicked)
+        clicked = !clicked
+    }
+
+    private fun setAnimation(clicked: Boolean){
+        val fab = view?.findViewById<FloatingActionButton>(R.id.fab)
+        val fabAdd = view?.findViewById<FloatingActionButton>(R.id.fab_add)
+        val fabPlay = view?.findViewById<FloatingActionButton>(R.id.fab_play)
+        val fabSave = view?.findViewById<FloatingActionButton>(R.id.fab_save)
+
+        if(!clicked){
+            fabAdd?.startAnimation(fromBottom)
+            fabPlay?.startAnimation(fromBottom)
+            fabSave?.startAnimation(fromBottom)
+            fab?.startAnimation(open)
+        }else{
+            fabAdd?.startAnimation(toBottom)
+            fabPlay?.startAnimation(toBottom)
+            fabSave?.startAnimation(toBottom)
+            fab?.startAnimation(close)
+        }
+    }
+
+    private fun setVisibility(clicked: Boolean){
+        val fabAdd = view?.findViewById<FloatingActionButton>(R.id.fab_add)
+        val fabPlay = view?.findViewById<FloatingActionButton>(R.id.fab_play)
+        val fabSave = view?.findViewById<FloatingActionButton>(R.id.fab_save)
+        if (!clicked){
+            fabAdd?.visibility = View.VISIBLE
+            fabPlay?.visibility = View.VISIBLE
+            fabSave?.visibility = View.VISIBLE
+        }else{
+            fabAdd?.visibility = View.INVISIBLE
+            fabPlay?.visibility = View.INVISIBLE
+            fabSave?.visibility = View.INVISIBLE
+        }
     }
 
 }
