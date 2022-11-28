667a7f873ff2d52ae5c0f47630d5a49ec79a0bc7
LucaPerf <l.perfetti2001@gmail.com>
2022-11-28 21:23:13 +0100

    changed the listview and implemented the methods

--- app/src/main/java/it/pdm/timers/fragments/TimerFragment.kt
+++ app/src/main/java/it/pdm/timers/fragments/TimerFragment.kt
@@ -1,12 +1,11 @@
 package it.pdm.timers.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import it.pdm.timers.CountdownActivity
-import it.pdm.timers.R
-import it.pdm.timers.SetTimeActivity
+import it.pdm.timers.*
 import kotlinx.android.synthetic.main.fragment_timer.*
 
 /**
@@ -15,6 +14,8 @@ import kotlinx.android.synthetic.main.fragment_timer.*
  * create an instance of this fragment.
  */
 class TimerFragment : Fragment() {
+    private lateinit var userArrayList : ArrayList<Timer>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,7 +28,20 @@ class TimerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btn_set_time.setOnClickListener {
+        val ntimer = arrayOf("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15")
+
+        val timet = arrayOf("00:01", "00:02", "00:03", "00:04", "00:05", "00:06", "00:07", "00:08", "00:09", "00:10",
+            "00:11", "00:12", "00:13", "00:14", "00:15")
+
+        userArrayList = ArrayList()
+        for (i in ntimer.indices){
+            val timer = Timer(ntimer[i], timet[i])
+            userArrayList.add(timer)
+        }
+
+        lv_timer.adapter = Adapter(this.requireActivity(), userArrayList)
+
+        /*btn_set_time.setOnClickListener {
             val intent = Intent(this.requireContext(), SetTimeActivity::class.java)
             startActivity(intent)
         }
@@ -35,7 +49,7 @@ class TimerFragment : Fragment() {
         btn_avvio.setOnClickListener {
             val intent = Intent(this.requireContext(), CountdownActivity::class.java)
             startActivity(intent)
-        }
+        }*/
 
     }
 
