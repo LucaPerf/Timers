efa22f5e00583e657a8398d34cd9705c4cc837d5
LucaPerf <l.perfetti2001@gmail.com>
2022-12-05 17:54:05 +0100

    changed parametries of class

--- app/src/main/java/it/pdm/timers/Timer.kt
+++ app/src/main/java/it/pdm/timers/Timer.kt
@@ -1,3 +1,6 @@
 package it.pdm.timers
 
-data class Timer(var NTimer : String, val timeTimer : String)
+//data class Timer(val NTimer: String, val timeTimer: String)
+
+data class Timer(val minuto2: String, val minuto1: String,
+                 val secondo2: String, val secondo1: String)
