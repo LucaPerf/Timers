1605bdb22775d1d593d03d1820a934be20c21e41
LucaPerf <l.perfetti2001@gmail.com>
2022-12-14 12:17:31 +0100

    added recyclerview list

--- app/src/main/java/it/pdm/timers/fragments/TimerSalvatiFragment.kt
+++ app/src/main/java/it/pdm/timers/fragments/TimerSalvatiFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import it.pdm.timers.AdapterRV
+import it.pdm.timers.Allenamenti
 import it.pdm.timers.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,43 +22,58 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TimerSalvatiFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var newRecyclerView: RecyclerView
+    private lateinit var newArrayList: ArrayList<Allenamenti>
+    lateinit var numbers: Array<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view : View = inflater.inflate(R.layout.fragment_timer_salvati, container, false)
+
+        numbers = arrayOf(
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20"
+        )
+
+        newRecyclerView = view.findViewById(R.id.recyclerview)
+        newRecyclerView.layoutManager = LinearLayoutManager(this.requireContext())
+        newRecyclerView.setHasFixedSize(true)
+
+        newArrayList = arrayListOf<Allenamenti>()
+        getUserdata()
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer_salvati, container, false)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TimerSalvatiFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TimerSalvatiFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun getUserdata() {
+        for(i in numbers.indices){
+            val allenamenti = Allenamenti(numbers[i])
+            newArrayList.add(allenamenti)
+        }
+
+        newRecyclerView.adapter = AdapterRV(newArrayList)
     }
+
+
 }
