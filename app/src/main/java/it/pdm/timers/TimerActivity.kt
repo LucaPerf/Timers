8780708806534e8503d792265b0f9dcf5206f06e
LucaPerf <l.perfetti2001@gmail.com>
2022-12-24 11:28:52 +0100

    added navigationview and drawerLayout

--- app/src/main/java/it/pdm/timers/TimerActivity.kt
+++ app/src/main/java/it/pdm/timers/TimerActivity.kt
@@ -1,24 +1,53 @@
 package it.pdm.timers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import it.pdm.timers.fragments.HomeFragment
 import it.pdm.timers.fragments.TimerFragment
 import it.pdm.timers.fragments.TimerSalvatiFragment
+import kotlinx.android.synthetic.main.activity_timer.*
 
 class TimerActivity : AppCompatActivity(), Communicator{
     private val timerFragment = TimerFragment()
     private val timerSalvatiFragment = TimerSalvatiFragment()
     private val homeFragment = HomeFragment()
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
         replaceFragment(timerFragment)
 
         val btn_navigation = findViewById<TextView>(R.id.bottom_navigation) as BottomNavigationView
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        //mostrare in alto a destra le impostazioni
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener{
+            it.isCheckable = true
+            when(it.itemId){
+                R.id.profile -> replaceFragment(homeFragment)
+                R.id.settings -> replaceFragment(homeFragment)
+                R.id.logout -> replaceFragment(homeFragment)
+            }
+            true
+        }
 
         btn_navigation.setOnItemSelectedListener{
             when(it.itemId){
@@ -30,11 +59,13 @@ class TimerActivity : AppCompatActivity(), Communicator{
         }
     }
 
+
     private fun replaceFragment(fragment: Fragment){
         val fragmentManager = supportFragmentManager
         val fragmentTransition = fragmentManager.beginTransaction()
         fragmentTransition.replace(R.id.fragment_container, fragment)
         fragmentTransition.commit()
+        drawerLayout.closeDrawers()
     }
 
     override fun passData(addItem: String) {
@@ -46,5 +77,14 @@ class TimerActivity : AppCompatActivity(), Communicator{
 
         //passiamo i dati al timerfragmentsalvti e apriamo il fragment
         transaction.replace(R.id.fragment_container, timerSalvatiFragment).commit()
+    }
+
+    //usata aprire la navigation drawer
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+
     }
 }
