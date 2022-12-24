4f4a9793ad10b9533256d3b44c0d61818375f882
LucaPerf <l.perfetti2001@gmail.com>
2022-12-24 12:16:13 +0100

    implemented fragment_profile

--- app/src/main/java/it/pdm/timers/fragments/ProfileFragment.kt
+++ app/src/main/java/it/pdm/timers/fragments/ProfileFragment.kt
@@ -1,10 +1,13 @@
 package it.pdm.timers.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import it.pdm.timers.PasswordDimenticataActivity
 import it.pdm.timers.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -23,7 +26,14 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val tv_forgot_password = view.findViewById<TextView>(R.id.tv_password_dimenticata)
+
+        tv_forgot_password.setOnClickListener {
+            val intent = Intent(this.requireContext(), PasswordDimenticataActivity::class.java)
+            startActivity(intent)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return view
     }
 }
