eb182c77380ab7bc50dfb45e6c243716a649249c
LucaPerf <l.perfetti2001@gmail.com>
2022-12-28 22:53:47 +0100

    implemented register button

--- app/src/main/java/it/pdm/timers/RegisterActivity.kt
+++ app/src/main/java/it/pdm/timers/RegisterActivity.kt
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -18,11 +23,39 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val nameandsurname = findViewById<TextInputEditText>(R.id.et_name)
+        val mail = findViewById<TextInputEditText>(R.id.et_email)
+        val username = findViewById<TextInputEditText>(R.id.et_username)
+        val password = findViewById<TextInputEditText>(R.id.et_password)
         val btn_Register = findViewById<Button>(R.id.btn_register)
 
         btn_Register.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+
+            val uasString = nameandsurname.text.toString()
+            val mailString = mail.text.toString()
+            val usernameString = username.text.toString()
+            val passwordString = password.text.toString()
+
+            //creazione del path che vedremo nel firbase
+           database = FirebaseDatabase.getInstance().getReference("Utenti")
+            val utenti = User(uasString, mailString, usernameString, passwordString)
+            //ogni username indicherà la persona all'interno del path nel firebase
+            database.child(usernameString).setValue(utenti).addOnSuccessListener {
+                nameandsurname.text?.clear()
+                mail.text?.clear()
+                username.text?.clear()
+                password.text?.clear()
+
+                Toast.makeText(this, "salvato con successo", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+
+            }.addOnFailureListener {
+                Toast.makeText(this, "Errore", Toast.LENGTH_SHORT).show()
+            }
+
+
         }
     }
 }
