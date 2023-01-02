e37ffdaf9068490e1749b8001fb0a5df870c9162
LucaPerf <l.perfetti2001@gmail.com>
2023-01-02 12:18:45 +0100

    implemented activity

--- app/src/main/java/it/pdm/timers/AllenamentoSalvatoActivity.kt
+++ app/src/main/java/it/pdm/timers/AllenamentoSalvatoActivity.kt
@@ -2,12 +2,48 @@ package it.pdm.timers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
-import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
 
 class AllenamentoSalvatoActivity : AppCompatActivity() {
+    private lateinit var ArrayTimer : ArrayList<Timer>
+    private lateinit var database : DatabaseReference
+    private lateinit var adapter : Adapter
+    var eventListener: ValueEventListener? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_allenamento_salvato)
+
+        var recyclerview = findViewById<RecyclerView>(R.id.recyclerviews)
+
+
+        database = FirebaseDatabase.getInstance().getReference("Timers")
+        ArrayTimer = ArrayList()
+        val gridLayoutManager = GridLayoutManager(this, 1)
+        recyclerview.layoutManager = gridLayoutManager
+
+        adapter = Adapter(this, ArrayTimer)
+        recyclerview.adapter = adapter
+
+        eventListener = database!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                ArrayTimer.clear()
+                for(itemSnaphot in snapshot.children){
+                    val timer = itemSnaphot.getValue(Timer::class.java)
+                    if(timer != null){
+                        ArrayTimer.add(timer)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
