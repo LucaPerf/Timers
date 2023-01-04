1116b66bc6556a0f8efa63d0958c2106cf321f92
LucaPerf <l.perfetti2001@gmail.com>
2023-01-04 15:26:01 +0100

    delete password

--- app/src/main/java/it/pdm/timers/User.kt
+++ app/src/main/java/it/pdm/timers/User.kt
@@ -1,8 +1,8 @@
 package it.pdm.timers
 
 //classe utilizzata per il salvataggio degli utenti nel database
-data class User(val NameAndSurname : String? = null, val email : String? = null,
-    val username : String? = null, val password : String? = null){
+data class User( val email : String? = null, val NameAndSurname : String? = null,
+    val username : String? = null){
 
 
 }
