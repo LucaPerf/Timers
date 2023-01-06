f9cd437af6807fe62ab8ab6cae9be47c73fdf07c
LucaPerf <l.perfetti2001@gmail.com>
2023-01-06 14:50:44 +0100

    added the ability to go back when the countdown is running

--- app/src/main/java/it/pdm/timers/CountdownActivity.kt
+++ app/src/main/java/it/pdm/timers/CountdownActivity.kt
@@ -1,5 +1,6 @@
 package it.pdm.timers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -11,6 +12,7 @@ import java.util.*
 class CountdownActivity : AppCompatActivity() {
     var txt_time: TextView? = null
     var img_play: ImageView? = null
+    var img_delete: ImageView? = null
     var progressbar: ProgressBar? = null
     var timerLengthMin: Int? = null
     var timerLengthSec: Int? = null
@@ -37,6 +39,13 @@ class CountdownActivity : AppCompatActivity() {
             }
         }
 
+        img_delete?.setOnClickListener {
+            val i = Intent(this, TimerActivity::class.java)
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(i)
+        }
+
         //start with time and progressbar complete
         updateCountDownText()
         setProgressBarValues()
@@ -46,6 +55,7 @@ class CountdownActivity : AppCompatActivity() {
         txt_time = findViewById(R.id.tv_countdown)
         progressbar = findViewById(R.id.progress_coundown)
         img_play = findViewById(R.id.iv_play)
+        img_delete = findViewById(R.id.tv_delete)
     }
 
     fun getData(){
