0e0be64389ede109b338eb65c56bc988c5a586dd
LucaPerf <l.perfetti2001@gmail.com>
2023-01-28 18:20:38 +0100

    changed putExtra in onBindViewHolder method

--- app/src/main/java/it/pdm/timers/AdapterRV.kt
+++ app/src/main/java/it/pdm/timers/AdapterRV.kt
@@ -17,9 +17,10 @@ class AdapterRV(private val context: Activity, private val allenamentiList: Arra
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.recCard.setOnClickListener {
+       holder.recCard.setOnClickListener {
             val intent = Intent(context, AllenamentoSalvatoActivity::class.java)
-            intent.putExtra("Timers", allenamentiList[holder.adapterPosition].number_recyclerview)
+            //intent.putExtra("Timers", allenamentiList[holder.adapterPosition].number_recyclerview)
+            intent.putExtra("Timers", holder.number_rv.text.toString())
             context.startActivity(intent)
         }
     }
@@ -27,15 +28,14 @@ class AdapterRV(private val context: Activity, private val allenamentiList: Arra
     override fun getItemCount(): Int {
         return allenamentiList.size
     }
-
 }
 
 class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
     var recCard : CardView
-    var name_rv : TextView
+    var number_rv : TextView
 
     init {
         recCard = itemView.findViewById(R.id.recAllenamenti)
-        name_rv = itemView.findViewById(R.id.tv_recyclerview_name)
+        number_rv = itemView.findViewById(R.id.tv_recyclerview_numbers)
     }
 }
